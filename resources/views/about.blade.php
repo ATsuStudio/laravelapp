8412e4fcc60f30513efb9c1d88f460461fb5ec54
ATsuStudio <tsyrenovalamga25@gmail.com>
2024-01-03 16:39:59 +0800

    about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.index')
 @section('main')
 <main>
-    this is laravel app 
+    <div class="px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4" src="https://mevn-public.s3-ap-southeast-1.amazonaws.com/marketenterprise.vn/wp-images/2020/06/12113245/Screen-Shot-2020-06-12-at-11.31.30-AM-1568x1447.png" alt="" width="72" height="57">
+        <h1 class="display-5 fw-bold">Laravel app</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">This is education project</p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <a href="/" type="button" class="btn btn-primary btn-lg px-4 gap-3">Home</a>
+            </div>
+        </div>
+    </div>
 </main>
 @endsection
